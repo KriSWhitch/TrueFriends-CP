cc1c9309a3040c05b6ae8946acbb23ade6c028d5
Alexei <krisfragerfeed@gmail.com>
2021-04-11 08:41:28 +0300

    feat: update

--- TrueFriendsApp/TrueFriendsApp/DB/DB.cs
+++ TrueFriendsApp/TrueFriendsApp/DB/DB.cs
@@ -12,10 +12,10 @@ namespace TrueFriendsApp
     public static class DB
     {
         private static string StringConnection = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+        private static SqlConnection cn_connection = new SqlConnection(StringConnection);
+
         public static SqlConnection GetDBConnection()
         {
-            SqlConnection cn_connection = new SqlConnection(StringConnection);
-
             if (cn_connection.State != ConnectionState.Open) cn_connection.Open();
 
             return cn_connection;
@@ -42,7 +42,6 @@ namespace TrueFriendsApp
 
         public static void CloseDBConnection()
         {
-            SqlConnection cn_connection = new SqlConnection(StringConnection);
             if (cn_connection.State != ConnectionState.Closed) cn_connection.Close();
         }
 
@@ -61,154 +60,143 @@ namespace TrueFriendsApp
 
         public static void CreateAdvert(string fullName, string shortName, int animalAge, decimal animalWeight, string kindOfAnimal, string description, byte[] pictureByteArray)
         {
-            using (SqlConnection sqlCon = new SqlConnection(StringConnection))
+            
+            cn_connection = GetDBConnection();
+            try
             {
-                try
-                {
-                    sqlCon.Open();
-                    SqlCommand command = new SqlCommand();
-                    command.Connection = sqlCon;
-                    command.CommandText = @"INSERT INTO Advert ([Advert_FullName], [Advert_ShortName], [Advert_AnimalAge], [Advert_AnimalWeight], [Advert_KindOfAnimal], [Advert_Description], [Advert_Image], [Advert_CreationDate]) 
-                                            VALUES (@Advert_FullName,@Advert_ShortName,@Advert_AnimalAge, @Advert_AnimalWeight, @Advert_KindOfAnimal, @Advert_Description, @Advert_Image, GETDATE())";
+                SqlCommand command = new SqlCommand();
+                command.Connection = cn_connection;
+                command.CommandText = @"INSERT INTO Advert ([Advert_FullName], [Advert_ShortName], [Advert_AnimalAge], [Advert_AnimalWeight], [Advert_KindOfAnimal], [Advert_Description], [Advert_Image], [Advert_CreationDate]) 
+                                        VALUES (@Advert_FullName,@Advert_ShortName,@Advert_AnimalAge, @Advert_AnimalWeight, @Advert_KindOfAnimal, @Advert_Description, @Advert_Image, GETDATE())";
 
-                    command.Parameters.Add("@Advert_FullName", SqlDbType.VarChar, 40);
-                    command.Parameters.Add("@Advert_ShortName", SqlDbType.VarChar, 20);
-                    command.Parameters.Add("@Advert_AnimalAge", SqlDbType.Int);
-                    command.Parameters.Add("@Advert_AnimalWeight", SqlDbType.Float);
-                    command.Parameters.Add("@Advert_KindOfAnimal", SqlDbType.VarChar, 20);
-                    command.Parameters.Add("@Advert_Description", SqlDbType.VarChar, 2000);
-                    command.Parameters.Add("@Advert_Image", SqlDbType.VarBinary);
+                command.Parameters.Add("@Advert_FullName", SqlDbType.VarChar, 40);
+                command.Parameters.Add("@Advert_ShortName", SqlDbType.VarChar, 20);
+                command.Parameters.Add("@Advert_AnimalAge", SqlDbType.Int);
+                command.Parameters.Add("@Advert_AnimalWeight", SqlDbType.Float);
+                command.Parameters.Add("@Advert_KindOfAnimal", SqlDbType.VarChar, 20);
+                command.Parameters.Add("@Advert_Description", SqlDbType.VarChar, 2000);
+                command.Parameters.Add("@Advert_Image", SqlDbType.VarBinary);
 
-                    command.Parameters["@Advert_FullName"].Value = fullName;
-                    command.Parameters["@Advert_ShortName"].Value = shortName;
-                    command.Parameters["@Advert_AnimalAge"].Value = animalAge;
-                    command.Parameters["@Advert_AnimalWeight"].Value = animalWeight;
-                    command.Parameters["@Advert_KindOfAnimal"].Value = kindOfAnimal;
-                    command.Parameters["@Advert_Description"].Value = description;
-                    command.Parameters["@Advert_Image"].Value = pictureByteArray;
+                command.Parameters["@Advert_FullName"].Value = fullName;
+                command.Parameters["@Advert_ShortName"].Value = shortName;
+                command.Parameters["@Advert_AnimalAge"].Value = animalAge;
+                command.Parameters["@Advert_AnimalWeight"].Value = animalWeight;
+                command.Parameters["@Advert_KindOfAnimal"].Value = kindOfAnimal;
+                command.Parameters["@Advert_Description"].Value = description;
+                command.Parameters["@Advert_Image"].Value = pictureByteArray;
 
-                    command.ExecuteNonQuery();
-                }
-                catch (Exception e)
-                {
-                    MessageBox.Show(e.Message);
-                }
+                command.ExecuteNonQuery();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
             }
         }
 
         public static void EditAdvert(int id, string fullName, string shortName, int animalAge, decimal animalWeight, string kindOfAnimal, string description, byte[] pictureByteArray)
         {
-            using (SqlConnection sqlCon = new SqlConnection(StringConnection))
+            cn_connection = GetDBConnection();
+            try
             {
-                try
-                {
-                    sqlCon.Open();
-                    SqlCommand command = new SqlCommand();
-                    command.Connection = sqlCon;
-                    command.CommandText = @"UPDATE Advert SET Advert_FullName = @Advert_FullName, Advert_ShortName = @Advert_ShortName, 
-                                            Advert_AnimalAge = @Advert_AnimalAge, Advert_AnimalWeight = @Advert_AnimalWeight, Advert_KindOfAnimal = @Advert_KindOfAnimal, 
-                                            Advert_Description = @Advert_Description, Advert_Image = @Advert_Image WHERE Advert_ID = @Advert_ID";
+                SqlCommand command = new SqlCommand();
+                command.Connection = cn_connection;
+                command.CommandText = @"UPDATE Advert SET Advert_FullName = @Advert_FullName, Advert_ShortName = @Advert_ShortName, 
+                                        Advert_AnimalAge = @Advert_AnimalAge, Advert_AnimalWeight = @Advert_AnimalWeight, Advert_KindOfAnimal = @Advert_KindOfAnimal, 
+                                        Advert_Description = @Advert_Description, Advert_Image = @Advert_Image WHERE Advert_ID = @Advert_ID";
 
-                    command.Parameters.Add("@Advert_ID", SqlDbType.Int);
-                    command.Parameters.Add("@Advert_FullName", SqlDbType.VarChar, 40);
-                    command.Parameters.Add("@Advert_ShortName", SqlDbType.VarChar, 20);
-                    command.Parameters.Add("@Advert_AnimalAge", SqlDbType.Int);
-                    command.Parameters.Add("@Advert_AnimalWeight", SqlDbType.Float);
-                    command.Parameters.Add("@Advert_KindOfAnimal", SqlDbType.VarChar, 20);
-                    command.Parameters.Add("@Advert_Description", SqlDbType.VarChar, 2000);
-                    command.Parameters.Add("@Advert_Image", SqlDbType.VarBinary);
+                command.Parameters.Add("@Advert_ID", SqlDbType.Int);
+                command.Parameters.Add("@Advert_FullName", SqlDbType.VarChar, 40);
+                command.Parameters.Add("@Advert_ShortName", SqlDbType.VarChar, 20);
+                command.Parameters.Add("@Advert_AnimalAge", SqlDbType.Int);
+                command.Parameters.Add("@Advert_AnimalWeight", SqlDbType.Float);
+                command.Parameters.Add("@Advert_KindOfAnimal", SqlDbType.VarChar, 20);
+                command.Parameters.Add("@Advert_Description", SqlDbType.VarChar, 2000);
+                command.Parameters.Add("@Advert_Image", SqlDbType.VarBinary);
 
-                    command.Parameters["@Advert_ID"].Value = id;
-                    command.Parameters["@Advert_FullName"].Value = fullName;
-                    command.Parameters["@Advert_ShortName"].Value = shortName;
-                    command.Parameters["@Advert_AnimalAge"].Value = animalAge;
-                    command.Parameters["@Advert_AnimalWeight"].Value = animalWeight;
-                    command.Parameters["@Advert_KindOfAnimal"].Value = kindOfAnimal;
-                    command.Parameters["@Advert_Description"].Value = description;
-                    command.Parameters["@Advert_Image"].Value = pictureByteArray;
+                command.Parameters["@Advert_ID"].Value = id;
+                command.Parameters["@Advert_FullName"].Value = fullName;
+                command.Parameters["@Advert_ShortName"].Value = shortName;
+                command.Parameters["@Advert_AnimalAge"].Value = animalAge;
+                command.Parameters["@Advert_AnimalWeight"].Value = animalWeight;
+                command.Parameters["@Advert_KindOfAnimal"].Value = kindOfAnimal;
+                command.Parameters["@Advert_Description"].Value = description;
+                command.Parameters["@Advert_Image"].Value = pictureByteArray;
 
-                    command.ExecuteNonQuery();
-                }
-                catch (Exception e)
-                {
-                    MessageBox.Show(e.Message);
-                }
+                command.ExecuteNonQuery();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
             }
         }
 
         public static void DeleteAdvert(int id)
         {
-            using (SqlConnection sqlCon = new SqlConnection(StringConnection))
+            cn_connection = GetDBConnection();
+            try
             {
-                try
-                {
-                    sqlCon.Open();
-                    SqlCommand command = new SqlCommand();
-                    command.Connection = sqlCon;
-                    command.CommandText = @"DELETE Advert WHERE Advert_ID = @Advert_ID";
-                    command.Parameters.Add("@Advert_ID", SqlDbType.Int);
-                    command.Parameters["@Advert_ID"].Value = id;
+                SqlCommand command = new SqlCommand();
+                command.Connection = cn_connection;
+                command.CommandText = @"DELETE Advert WHERE Advert_ID = @Advert_ID";
+                command.Parameters.Add("@Advert_ID", SqlDbType.Int);
+                command.Parameters["@Advert_ID"].Value = id;
 
-                    command.ExecuteNonQuery();
-                }
-                catch (Exception e)
-                {
-                    MessageBox.Show(e.Message);
-                }
+                command.ExecuteNonQuery();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
             }
         }
 
         public static List<Advert> GetAdverts()
         {
             List<Advert> adverts = new List<Advert>();
-            using (SqlConnection sqlCon = new SqlConnection(StringConnection))
+            cn_connection = GetDBConnection();
+            try
             {
-                try
+                SqlCommand command = new SqlCommand();
+                command.Connection = cn_connection;
+                command.CommandText = @"Select Advert_ID, Advert_FullName, Advert_ShortName, Advert_AnimalAge, Advert_AnimalWeight, Advert_KindOfAnimal, Advert_Description, Advert_Image, Advert_Creator, Advert_CreationDate From Advert";
+
+
+                SqlDataReader info = command.ExecuteReader();
+                object advertCreator = null, id = null, fullName = null, shortName = null, animalAge = null, animalWeight = null, kindOfAnimal = null, description = null, image = null, creationDate = null;
+
+                while (info.Read())
                 {
-                    sqlCon.Open();
-                    SqlCommand command = new SqlCommand();
-                    command.Connection = sqlCon;
-                    command.CommandText = @"Select Advert_ID, Advert_FullName, Advert_ShortName, Advert_AnimalAge, Advert_AnimalWeight, Advert_KindOfAnimal, Advert_Description, Advert_Image, Advert_Creator, Advert_CreationDate From Advert";
-
-
-                    SqlDataReader info = command.ExecuteReader();
-                    object advertCreator = null, id = null, fullName = null, shortName = null, animalAge = null, animalWeight = null, kindOfAnimal = null, description = null, image = null, creationDate = null;
-
-                    while (info.Read())
-                    {
-                        id = info["Advert_ID"];
-                        fullName = info["Advert_FullName"];
-                        shortName = info["Advert_ShortName"];
-                        animalAge = info["Advert_AnimalAge"];
-                        animalWeight = info["Advert_AnimalWeight"];
-                        kindOfAnimal = info["Advert_KindOfAnimal"];
-                        image = info["Advert_Image"];
-                        description = info["Advert_Description"];
-                        advertCreator = info["Advert_Creator"];
-                        creationDate = info["Advert_CreationDate"];
-                        adverts.Add(
-                            new Advert()
-                            {
-                                ID = Convert.ToInt32(id),
-                                FullName = Convert.ToString(fullName),
-                                ShortName = Convert.ToString(shortName),
-                                AnimalAge = Convert.ToInt32(animalAge),
-                                AnimalWeight = Convert.ToDecimal(animalWeight),
-                                KindOfAnimal = Convert.ToString(kindOfAnimal),
-                                Image = new Picture() { PictureByteArray = (byte[])image },
-                                Description = Convert.ToString(description),
-                                AdvertCreator = Convert.ToString(advertCreator),
-                                CreationDate = Convert.ToString(creationDate)
-                            }
-                        );
-                    }
-                    return adverts;
+                    id = info["Advert_ID"];
+                    fullName = info["Advert_FullName"];
+                    shortName = info["Advert_ShortName"];
+                    animalAge = info["Advert_AnimalAge"];
+                    animalWeight = info["Advert_AnimalWeight"];
+                    kindOfAnimal = info["Advert_KindOfAnimal"];
+                    image = info["Advert_Image"];
+                    description = info["Advert_Description"];
+                    advertCreator = info["Advert_Creator"];
+                    creationDate = info["Advert_CreationDate"];
+                    adverts.Add(
+                        new Advert()
+                        {
+                            ID = Convert.ToInt32(id),
+                            FullName = Convert.ToString(fullName),
+                            ShortName = Convert.ToString(shortName),
+                            AnimalAge = Convert.ToInt32(animalAge),
+                            AnimalWeight = Convert.ToDecimal(animalWeight),
+                            KindOfAnimal = Convert.ToString(kindOfAnimal),
+                            Image = new Picture() { PictureByteArray = (byte[])image },
+                            Description = Convert.ToString(description),
+                            AdvertCreator = Convert.ToString(advertCreator),
+                            CreationDate = Convert.ToString(creationDate)
+                        }
+                    );
                 }
-                catch (Exception e)
-                {
-                    MessageBox.Show(e.Message);
-                    return adverts;
-                }
+                return adverts;
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+                return adverts;
             }
         }
 
